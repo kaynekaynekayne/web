dd29ece587f0ba559cdd8383b52652d93267fbbf
kaynekaynekayne <theygoon@gmail.com>
2022-10-29 00:16:17 +0900

    intro style

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AboutMe from "./pages/aboutMe";
+import { GlobalStyle } from "./style/globalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
